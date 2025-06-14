e00a4e38f86bc17d201dbcbd3a88cc6221a1b65d
kyumin7697 <kyumin.lee@my.jcu.edu.au>
2025-06-14 23:20:38 +0800

    add prac 4 quick pick pseudocode

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,18 @@
+"""
+MIN = 1
+MAX = 45
+NUMBERS_PER_PICK = 6
+
+get number_of_picks
+
+for i in range(num_picks)
+numbers = []
+    while len(numbers) < NUMBERS_PER_PICK
+        number = random.randint(MIN, MAX)
+        if number not in numbers
+            numbers.append(number)
+    numbers.sort()
+    print number for n in numbers
+"""
+
+
