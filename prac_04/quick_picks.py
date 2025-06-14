187e1daaa768a18e3426d3d01134cecc7ce9d7df
kyumin7697 <kyumin.lee@my.jcu.edu.au>
2025-06-14 23:46:43 +0800

    add prac 4 quick pick code

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -15,4 +15,19 @@ numbers = []
     print number for n in numbers
 """
 
+import random
 
+MIN = 1
+MAX = 45
+NUMBERS_PER_PICK = 6
+
+num_picks = int(input("How many quick picks? "))
+
+for i in range(num_picks):
+    numbers = []
+    while len(numbers) < NUMBERS_PER_PICK:
+        number = random.randint(MIN, MAX)
+        if number not in numbers:
+            numbers.append(number)
+    numbers.sort()
+    print(" ".join(f"{n:2}" for n in numbers))
