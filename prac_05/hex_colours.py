72a8d8f2173b2a1bdaed2ba2ed6bbbc202f8e9ae
kyumin7697 <kyumin.lee@my.jcu.edu.au>
2025-06-20 23:24:31 +0800

    add prac 5 hex_colours test first version, it can't read the lower letter in dictionary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -14,4 +14,12 @@ COLOUR_TO_HEX = {
     "BlanchedAlmond": "#ffebcd",
     "Blue": "#0000ff"
 }
+print(COLOUR_TO_HEX)
 
+colour_name = input("Enter a colour name: ").lower()
+while colour_name != "":
+    try:
+        print(f"{colour_name.title()} has the code {COLOUR_TO_HEX[colour_name]}")
+    except KeyError:
+        print("Invalid colour name")
+    colour_name = input("Enter a colour name: ").lower()
