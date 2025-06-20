b0929bd98e4e830d73cd692a56cd166462f7928a
kyumin7697 <kyumin.lee@my.jcu.edu.au>
2025-06-20 23:14:22 +0800

    add prac 5 hex_colours make the color dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+"""
+hex_colours.py
+"""
+
+COLOUR_TO_HEX = {
+    "AliceBlue": "#f0f8ff",
+    "AntiqueWhite": "#faebd7",
+    "Aqua": "#00ffff",
+    "Aquamarine": "#7fffd4",
+    "Azure": "#f0ffff",
+    "Beige": "#f5f5dc",
+    "Bisque": "#ffe4c4",
+    "Black": "#000000",
+    "BlanchedAlmond": "#ffebcd",
+    "Blue": "#0000ff"
+}
+
