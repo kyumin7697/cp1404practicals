8db8c5fc1dda05187d0bab4438051e39d3520072
kyumin7697 <kyumin.lee@my.jcu.edu.au>
2025-06-21 11:04:45 +0800

    add prac 5 Word Occurrences Calculated by the number of occurrences of each words in the dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,10 +3,19 @@ Word Occurrences
 Estimate: 40 minutes
 Actual:
 """
-
+#Text: this is a collection of words of nice words this is a fun thing it is
 def main():
     text = input("Text: ")
     words = text.split()
-    print(words)
+    word_to_count = {}
+
+    for word in words:
+        word = word.lower()
+        if word in word_to_count:
+            word_to_count[word] += 1
+        else:
+            word_to_count[word] = 1
+
+    print(word_to_count)
 
 main()
